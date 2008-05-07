68d96446e1d4aa3e13ef4d8d869bdc0113e5e8b8
Gilmar P.S.L <gilmatryx@gmail.com>
2008-05-07 01:26:53 +0000

    '''TICKET:'''  none
    '''RESUMO:''' EXEMPLE
    
     * Exemplo para geração de boletos.

--- src/br/com/nordestefomento/jrimum/bopepo/view/BoletoViewer.java
+++ src/br/com/nordestefomento/jrimum/bopepo/view/BoletoViewer.java
@@ -116,19 +116,19 @@ public class BoletoViewer extends ACurbitaObject{
 	
 	public static void main (String[] args) throws DocumentException, IllegalArgumentException, IOException, NotSuporttedBancoException, NotSuporttedCampoLivreException {
 		
-		Pessoa sacado = new Pessoa("Fulano da Silva Sauro Super Feliz com a Compra", "222.222.222-22");
+		Pessoa sacado = new Pessoa("Javadeveloper Pronto Para Férias", "222.222.222-22");
 		
 		Endereco endereco = new Endereco();
 		endereco.setUf(EnumUnidadeFederativa.RN);
 		endereco.setLocalidade(new Localidade("Natal"));
 		endereco.setCep(new CEP("59064-120"));
 		endereco.setBairro("Grande Centro");
-		endereco.setLogradouro(new Logradouro("Rua Poeta das Princesas"));
+		endereco.setLogradouro(new Logradouro("Rua poeta dos programas"));
 		endereco.setNumero("1");
 		
 		sacado.addEndereco(endereco);
 		
-		Pessoa cedente = new Pessoa("Empresa Lucrativa S.A para Todo Sempre Ilimitada", "00.000.208/0001-00");
+		Pessoa cedente = new Pessoa("PROJETO JRimum", "00.000.208/0001-00");
 		
 		IBanco banco = EnumBancos.BANCO_DO_BRASIL.newInstance();
 		ContaBancaria contaBancaria = new ContaBancaria(banco);
@@ -139,14 +139,14 @@ public class BoletoViewer extends ACurbitaObject{
 
 		cedente.addContaBancaria(contaBancaria);
 		
-		Pessoa sacadorAvalista = new Pessoa("Banco do Brasil", "00.000.000/0001-91");
+		Pessoa sacadorAvalista = new Pessoa("Nordeste Fomento Mercantil", "00.000.000/0001-91");
 		
 		Endereco endereco2 = new Endereco();
 		endereco2.setUf(EnumUnidadeFederativa.DF);
 		endereco2.setLocalidade(new Localidade("Brasília"));
 		endereco2.setCep(new CEP("00000-000"));
 		endereco2.setBairro("Grande Centro");
-		endereco2.setLogradouro(new Logradouro("Rua Principal Para Sempre"));
+		endereco2.setLogradouro(new Logradouro("Rua Eternamente Principal"));
 		endereco2.setNumero("001");
 
 		sacadorAvalista.addEndereco(endereco2);
@@ -155,7 +155,7 @@ public class BoletoViewer extends ACurbitaObject{
 		titulo.setNumeroDoDocumento("123456789");
 		titulo.setNossoNumero("1234567890");
 		titulo.setDigitoDoNossoNumero("5");
-		titulo.setValor(BigDecimal.valueOf(100.23));
+		titulo.setValor(BigDecimal.valueOf(0.23));
 		titulo.setDataDoDocumento(new Date());
 		titulo.setDataDoVencimento(new Date());
 		titulo.setTipoDeDocumento(EnumTitulo.DM_DUPLICATA_MERCANTIL);
@@ -167,22 +167,22 @@ public class BoletoViewer extends ACurbitaObject{
 		
 		boleto.setInsturcaoAoSacado("Senhor sacado, sabemos sim que o valor cobrado não é o esperado, aproveite o DESCONTÃO!");
 		
-		boleto.setInstrucao1("PARA PAGAMENTO 1 até xx/xx/xxxx COBRAR O VALOR DE: R$ YY,YY");
-		boleto.setInstrucao2("PARA PAGAMENTO 2 até xx/xx/xxxx COBRAR O VALOR DE: R$ YY,YY");
-		boleto.setInstrucao3("PARA PAGAMENTO 3 até xx/xx/xxxx COBRAR O VALOR DE: R$ YY,YY");
-		boleto.setInstrucao4("PARA PAGAMENTO 4 até xx/xx/xxxx COBRAR O VALOR DE: R$ YY,YY");
-		boleto.setInstrucao5("PARA PAGAMENTO 5 até xx/xx/xxxx COBRAR O VALOR DE: R$ YY,YY");
-		boleto.setInstrucao6("PARA PAGAMENTO 6 até xx/xx/xxxx COBRAR O VALOR DE: R$ YY,YY");
-		boleto.setInstrucao7("PARA PAGAMENTO 7 até xx/xx/xxxx COBRAR O VALOR DE: R$ YY,YY");
+		boleto.setInstrucao1("PARA PAGAMENTO 1 até Hoje não cobrar nada!");
+		boleto.setInstrucao2("PARA PAGAMENTO 2 até Amanhã Não cobre!");
+		boleto.setInstrucao3("PARA PAGAMENTO 3 até Depois de amanhã, OK, não cobre.");
+		boleto.setInstrucao4("PARA PAGAMENTO 4 até 04/xx/xxxx de 4 dias atrás COBRAR O VALOR DE: R$ 01,00");
+		boleto.setInstrucao5("PARA PAGAMENTO 5 até 05/xx/xxxx COBRAR O VALOR DE: R$ 02,00");
+		boleto.setInstrucao6("PARA PAGAMENTO 6 até 06/xx/xxxx COBRAR O VALOR DE: R$ 03,00");
+		boleto.setInstrucao7("PARA PAGAMENTO 7 até xx/xx/xxxx COBRAR O VALOR QUE VOCÊ QUISER!");
 		boleto.setInstrucao8("APÓS o Vencimento, Pagável Somente na Rede X.");
 		
 		BoletoViewer bViewer = new BoletoViewer(boleto);
-		bViewer.getAsPDF("testeBoletoPdfFromPdf.pdf");
+		bViewer.getAsPDF("BoletoTeste.pdf");
 		
-		 java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+		java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
 		 
 		try {
-			desktop.open(new File("testeBoletoPdfFromPdf.pdf"));
+			desktop.open(new File("BoletoTeste.pdf"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
