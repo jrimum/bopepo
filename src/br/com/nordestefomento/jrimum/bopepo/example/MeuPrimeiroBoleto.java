6be1fcdbc56734eb554bf41bc09e076f7b5bd837
Misael Barreto <misaelbarreto@gmail.com>
2008-06-14 16:31:52 +0000

    '''TICKET:''' #NONE
    '''RESUMO:''' Ajustes de texto e alinhamento na classe MeuPrimeiroBoleto.java, 
    base para o tutorial de geração de  boletos bancários.

--- src/br/com/nordestefomento/jrimum/bopepo/example/MeuPrimeiroBoleto.java
+++ src/br/com/nordestefomento/jrimum/bopepo/example/MeuPrimeiroBoleto.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import com.lowagie.text.DocumentException;
+
 import br.com.nordestefomento.jrimum.bopepo.Boleto;
 import br.com.nordestefomento.jrimum.bopepo.EnumBancos;
 import br.com.nordestefomento.jrimum.bopepo.campolivre.NotSuporttedBancoException;
@@ -25,36 +27,15 @@ import br.com.nordestefomento.jrimum.domkee.type.EnumUnidadeFederativa;
 import br.com.nordestefomento.jrimum.domkee.type.Localidade;
 import br.com.nordestefomento.jrimum.domkee.type.Logradouro;
 
-import com.lowagie.text.DocumentException;
 
 
-
-/**
- * 
- * <p>
- * Classe exemplo que mostra a geração de boletos de vários bancos.<br/>
- * Para cada banco o código se repete propositalmente, afim de facilitar
- * o entendimento por parte do usuário.
- * </p>
- * 
- * 
- * @author <a href="http://gilmatryx.googlepages.com/">Gilmar P.S.L.</a>
- * @author Misael Barreto
- * 
- * @since 0.2 
- * 
- * @version 0.2
- */
-	
 public class MeuPrimeiroBoleto {
 
-	public static void main (String[] args) throws DocumentException, IllegalArgumentException, IOException, NotSuporttedBancoException, NotSuporttedCampoLivreException {
-		gereBoletoBancoDoBrasil();
-	}
-	
-	
-	private static void gereBoletoBancoDoBrasil() throws IllegalArgumentException, IOException, DocumentException {
-		/* 
+	public static void main (String[] args) throws DocumentException,
+	IllegalArgumentException, IOException, NotSuporttedBancoException,
+	NotSuporttedCampoLivreException {
+		
+ 		/* 
 		 * INFORMANDO DADOS SOBRE O CEDENTE.
 		 * */
 		Pessoa cedente = new Pessoa("PROJETO JRimum", "00.000.208/0001-00");
@@ -67,10 +48,14 @@ public class MeuPrimeiroBoleto {
 		contaBancariaCed.setNumeroDaConta(new NumeroDaConta(6789, "12"));
 		cedente.addContaBancaria(contaBancariaCed);		
 		
+		
+		
+		
 		/* 
 		 * INFORMANDO DADOS SOBRE O SACADO.
 		 * */
-		Pessoa sacado = new Pessoa("JavaDeveloper Pronto Para Férias", "222.222.222-22");
+		Pessoa sacado = new Pessoa("JavaDeveloper Pronto Para Férias",
+				"222.222.222-22");
 
 		// Informando o endereço do sacado.
 		Endereco enderecoSac = new Endereco();
@@ -82,10 +67,14 @@ public class MeuPrimeiroBoleto {
 		enderecoSac.setNumero("1");
 		sacado.addEndereco(enderecoSac);
 		
+		
+		
+		
 		/* 
 		 * INFORMANDO DADOS SOBRE O SACADOR AVALISTA.
 		 * */
-		Pessoa sacadorAvalista = new Pessoa("Nordeste Fomento Mercantil", "00.000.000/0001-91");
+		Pessoa sacadorAvalista = new Pessoa("Nordeste Fomento Mercantil",
+				"00.000.000/0001-91");
 		
 		// Informando o endereço do sacador avalista. 
 		Endereco enderecoSacAval = new Endereco();
@@ -97,10 +86,13 @@ public class MeuPrimeiroBoleto {
 		enderecoSacAval.setNumero("001");
 		sacadorAvalista.addEndereco(enderecoSacAval);
 
+		
+		
+		
 		/* 
 		 * INFORMANDO OS DADOS SOBRE O TÍTULO.
 		 * */		
-		Titulo titulo = new Titulo(contaBancariaCed, sacado, cedente, sacadorAvalista);
+		Titulo titulo = new Titulo(contaBancariaCed, sacado, cedente,sacadorAvalista);
 		titulo.setNumeroDoDocumento("123456789");
 		titulo.setNossoNumero("1234567890");
 		titulo.setDigitoDoNossoNumero("5");
@@ -110,20 +102,31 @@ public class MeuPrimeiroBoleto {
 		titulo.setTipoDeDocumento(EnumTitulo.DM_DUPLICATA_MERCANTIL);
 		titulo.setAceite(EnumAceite.A);
 
+		
+		
 		/* 
 		 * INFORMANDO OS DADOS SOBRE O BOLETO.
 		 * */
 		Boleto boleto = new Boleto(titulo);
-		boleto.setLocalPagamento("Pagável preferencialmente na Rede X ou em qualquer Banco até o Vencimento.");
-		boleto.setInstrucaoAoSacado("Senhor sacado, sabemos sim que o valor cobrado não é o esperado, aproveite o DESCONTÃO!");
+		boleto.setLocalPagamento("Pagável preferencialmente na Rede X ou em " +
+				"qualquer Banco até o Vencimento.");
+		boleto.setInstrucaoAoSacado("Senhor sacado, sabemos sim que o valor " +
+				"cobrado não é o esperado, aproveite o DESCONTÃO!");
 		boleto.setInstrucao1("PARA PAGAMENTO 1 até Hoje não cobrar nada!");
 		boleto.setInstrucao2("PARA PAGAMENTO 2 até Amanhã Não cobre!");
 		boleto.setInstrucao3("PARA PAGAMENTO 3 até Depois de amanhã, OK, não cobre.");
-		boleto.setInstrucao4("PARA PAGAMENTO 4 até 04/xx/xxxx de 4 dias atrás COBRAR O VALOR DE: R$ 01,00");
-		boleto.setInstrucao5("PARA PAGAMENTO 5 até 05/xx/xxxx COBRAR O VALOR DE: R$ 02,00");
-		boleto.setInstrucao6("PARA PAGAMENTO 6 até 06/xx/xxxx COBRAR O VALOR DE: R$ 03,00");
-		boleto.setInstrucao7("PARA PAGAMENTO 7 até xx/xx/xxxx COBRAR O VALOR QUE VOCÊ QUISER!");
+		boleto.setInstrucao4("PARA PAGAMENTO 4 até 04/xx/xxxx de 4 dias atrás " +
+				"COBRAR O VALOR DE: R$ 01,00");
+		boleto.setInstrucao5("PARA PAGAMENTO 5 até 05/xx/xxxx COBRAR O VALOR " +
+				"DE: R$ 02,00");
+		boleto.setInstrucao6("PARA PAGAMENTO 6 até 06/xx/xxxx COBRAR O VALOR " +
+				"DE: R$ 03,00");
+		boleto.setInstrucao7("PARA PAGAMENTO 7 até xx/xx/xxxx COBRAR O VALOR " +
+				"QUE VOCÊ QUISER!");
 		boleto.setInstrucao8("APÓS o Vencimento, Pagável Somente na Rede X.");
+		
+		
+		
 		
 		/* 
 		 * GERANDO O BOLETO BANCÁRIO.
@@ -136,7 +139,7 @@ public class MeuPrimeiroBoleto {
 		// pasta do projeto. Outros exemplos:
 		// WINDOWS: boletoViewer.getAsPDF("C:/Temp/MeuBoleto.pdf");
 		// LINUX: boletoViewer.getAsPDF("/home/temp/MeuBoleto.pdf");
-		File arquivoPdf = boletoViewer.getAsPDF("MeuPrimeiroBoleto_BoletoBancoDoBrasil");
+		File arquivoPdf = boletoViewer.getAsPDF("MeuPrimeiroBoleto");
 		
 		// Mostrando o boleto gerado na tela.
 		mostreBoletoNaTela(arquivoPdf);
