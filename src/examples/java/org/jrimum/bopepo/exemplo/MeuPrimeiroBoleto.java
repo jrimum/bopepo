92e351dd837c13835ef75be628f1a34ffd677f0c
Rômulo Augusto <romulomail@gmail.com>
2010-11-03 11:52:36 +0000

    [wiki:Componente/Documentacao/Tutoriais Tutoriais]:  * Recriação do tutorial de Geração de Boleto Bancário

--- src/examples/java/org/jrimum/bopepo/exemplo/MeuPrimeiroBoleto.java
+++ src/examples/java/org/jrimum/bopepo/exemplo/MeuPrimeiroBoleto.java
@@ -34,21 +34,19 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import org.jrimum.bopepo.BancosSuportados;
 import org.jrimum.bopepo.Boleto;
 import org.jrimum.bopepo.view.BoletoViewer;
 import org.jrimum.domkee.comum.pessoa.endereco.CEP;
 import org.jrimum.domkee.comum.pessoa.endereco.Endereco;
 import org.jrimum.domkee.comum.pessoa.endereco.UnidadeFederativa;
-import org.jrimum.domkee.financeiro.banco.Banco;
 import org.jrimum.domkee.financeiro.banco.febraban.Agencia;
 import org.jrimum.domkee.financeiro.banco.febraban.Carteira;
 import org.jrimum.domkee.financeiro.banco.febraban.Cedente;
 import org.jrimum.domkee.financeiro.banco.febraban.ContaBancaria;
-import org.jrimum.domkee.financeiro.banco.febraban.Modalidade;
 import org.jrimum.domkee.financeiro.banco.febraban.NumeroDaConta;
 import org.jrimum.domkee.financeiro.banco.febraban.Sacado;
 import org.jrimum.domkee.financeiro.banco.febraban.SacadorAvalista;
-import org.jrimum.domkee.financeiro.banco.febraban.TipoDeCobranca;
 import org.jrimum.domkee.financeiro.banco.febraban.TipoDeTitulo;
 import org.jrimum.domkee.financeiro.banco.febraban.Titulo;
 import org.jrimum.domkee.financeiro.banco.febraban.Titulo.EnumAceite;
@@ -58,7 +56,7 @@ import org.jrimum.domkee.financeiro.banco.febraban.Titulo.EnumAceite;
  * Exemplo de código para geração de um boleto simples.
  * </p>
  * <p>
- * Utiliza dados genéricos (fictícios), como o banco e o campo livre.
+ * Utiliza o Banco Bradesco como exemplo, já que possui um implementação simples.
  * </p>
  * 
  * @author <a href="http://gilmatryx.googlepages.com/">Gilmar P.S.L</a>
@@ -77,16 +75,6 @@ public class MeuPrimeiroBoleto {
 		 * INFORMANDO DADOS SOBRE O CEDENTE.
 		 */
 		Cedente cedente = new Cedente("PROJETO JRimum", "00.000.208/0001-00");
-
-		// Informando dados sobre a conta bancária do cendente.
-		Banco banco = new JRimumBank();
-		ContaBancaria contaBancariaCed = new ContaBancaria(banco);
-		contaBancariaCed.setBanco(banco);
-		contaBancariaCed.setNumeroDaConta(new NumeroDaConta(123456, "0"));
-		contaBancariaCed.setCarteira(new Carteira(123, TipoDeCobranca.SEM_REGISTRO));
-		contaBancariaCed.setModalidade(new Modalidade(4));
-		contaBancariaCed.setAgencia(new Agencia(1234, "1"));
-		cedente.addContaBancaria(contaBancariaCed);
 
 		/*
 		 * INFORMANDO DADOS SOBRE O SACADO.
@@ -112,7 +100,7 @@ public class MeuPrimeiroBoleto {
 		Endereco enderecoSacAval = new Endereco();
 		enderecoSacAval.setUF(UnidadeFederativa.DF);
 		enderecoSacAval.setLocalidade("Brasília");
-		enderecoSacAval.setCep(new CEP("00000-000"));
+		enderecoSacAval.setCep(new CEP("59000-000"));
 		enderecoSacAval.setBairro("Grande Centro");
 		enderecoSacAval.setLogradouro("Rua Eternamente Principal");
 		enderecoSacAval.setNumero("001");
@@ -121,7 +109,14 @@ public class MeuPrimeiroBoleto {
 		/*
 		 * INFORMANDO OS DADOS SOBRE O TÍTULO.
 		 */
-		Titulo titulo = new Titulo(contaBancariaCed, sacado, cedente, sacadorAvalista);
+		
+		// Informando dados sobre a conta bancária do título.
+		ContaBancaria contaBancaria = new ContaBancaria(BancosSuportados.BANCO_BRADESCO.create());
+		contaBancaria.setNumeroDaConta(new NumeroDaConta(123456, "0"));
+		contaBancaria.setCarteira(new Carteira(30));
+		contaBancaria.setAgencia(new Agencia(1234, "1"));
+		
+		Titulo titulo = new Titulo(contaBancaria, sacado, cedente, sacadorAvalista);
 		titulo.setNumeroDoDocumento("123456");
 		titulo.setNossoNumero("993456789");
 		titulo.setDigitoDoNossoNumero("5");
@@ -139,24 +134,19 @@ public class MeuPrimeiroBoleto {
 		/*
 		 * INFORMANDO OS DADOS SOBRE O BOLETO.
 		 */
-		Boleto boleto = new Boleto(titulo, new CampoLivreJRimumBank(titulo));
+		Boleto boleto = new Boleto(titulo);
 		
-		boleto.setLocalPagamento("Pagável preferencialmente na Rede X ou em "
-				+ "qualquer Banco até o Vencimento.");
-		boleto.setInstrucaoAoSacado("Senhor sacado, sabemos sim que o valor "
-				+ "cobrado não é o esperado, aproveite o DESCONTÃO!");
+		boleto.setLocalPagamento("Pagável preferencialmente na Rede X ou em " +
+				"qualquer Banco até o Vencimento.");
+		boleto.setInstrucaoAoSacado("Senhor sacado, sabemos sim que o valor " +
+				"cobrado não é o esperado, aproveite o DESCONTÃO!");
 		boleto.setInstrucao1("PARA PAGAMENTO 1 até Hoje não cobrar nada!");
 		boleto.setInstrucao2("PARA PAGAMENTO 2 até Amanhã Não cobre!");
-		boleto
-				.setInstrucao3("PARA PAGAMENTO 3 até Depois de amanhã, OK, não cobre.");
-		boleto.setInstrucao4("PARA PAGAMENTO 4 até 04/xx/xxxx de 4 dias atrás "
-				+ "COBRAR O VALOR DE: R$ 01,00");
-		boleto.setInstrucao5("PARA PAGAMENTO 5 até 05/xx/xxxx COBRAR O VALOR "
-				+ "DE: R$ 02,00");
-		boleto.setInstrucao6("PARA PAGAMENTO 6 até 06/xx/xxxx COBRAR O VALOR "
-				+ "DE: R$ 03,00");
-		boleto.setInstrucao7("PARA PAGAMENTO 7 até xx/xx/xxxx COBRAR O VALOR "
-				+ "QUE VOCÊ QUISER!");
+		boleto.setInstrucao3("PARA PAGAMENTO 3 até Depois de amanhã, OK, não cobre.");
+		boleto.setInstrucao4("PARA PAGAMENTO 4 até 04/xx/xxxx de 4 dias atrás COBRAR O VALOR DE: R$ 01,00");
+		boleto.setInstrucao5("PARA PAGAMENTO 5 até 05/xx/xxxx COBRAR O VALOR DE: R$ 02,00");
+		boleto.setInstrucao6("PARA PAGAMENTO 6 até 06/xx/xxxx COBRAR O VALOR DE: R$ 03,00");
+		boleto.setInstrucao7("PARA PAGAMENTO 7 até xx/xx/xxxx COBRAR O VALOR QUE VOCÊ QUISER!");
 		boleto.setInstrucao8("APÓS o Vencimento, Pagável Somente na Rede X.");
 
 		/*
@@ -190,7 +180,5 @@ public class MeuPrimeiroBoleto {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
-
 }
