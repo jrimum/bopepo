259389da448996d84377fe33d91abffc14771b3f
Gilmar P.S.L <gilmatryx@gmail.com>
2010-11-24 02:10:43 +0000

    Revisão do código  * Adição de validações  * Padronizção javadoc  * Melhoria javadoc

--- src/main/java/org/jrimum/bopepo/campolivre/CLBradesco.java
+++ src/main/java/org/jrimum/bopepo/campolivre/CLBradesco.java
@@ -30,52 +30,59 @@
 
 package org.jrimum.bopepo.campolivre;
 
-import org.jrimum.domkee.financeiro.banco.febraban.ContaBancaria;
 import org.jrimum.domkee.financeiro.banco.febraban.Titulo;
 import org.jrimum.utilix.text.Field;
 import org.jrimum.utilix.text.Filler;
 
 /**
- * 
+ * <p>
  * O campo livre do bradesco deve seguir esta forma:
+ * </p>
  * 
- * <table border="1" cellpadding="0" cellspacing="0" style="border-collapse:
- * collapse" bordercolor="#111111" width="60%" id="campolivre">
- * <tr> <thead>
- * <th >Posição </th>
- * <th >Tamanho</th>
- * <th >Picture</th>
- * <th >Conteúdo</th>
- * </thead> </tr>
+ * <table border="1" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="100%" id="campolivre">
+ * <thead bgcolor="#DEDEDE">
+ * <tr>
+ * <th>Posição</th>
+ * <th>Tamanho</th>
+ * <th>Picture</th>
+ * <th>Conteúdo (terminologia padrão)</th>
+ * <th>Conteúdo (terminologia do banco)</th>
+ * </tr>
+ * </thead> <tbody style="text-align:center">
  * <tr>
  * <td >20-23</td>
  * <td >4</td>
- * <td >9(4) </td>
- * <td >Código da Agência (sem dígito)</td>
+ * <td >9(4)</td>
+ * <td style="text-align:left;padding-left:10">Agência Cedente (Sem o digito verificador, completar com zeros a esquerda quando necessário)</td>
+ * <td style="text-align:left;padding-left:10">Código da Agência (sem dígito)</td>
  * </tr>
  * <tr>
  * <td >24-25</td>
  * <td >2</td>
- * <td >9(2) </td>
- * <td >Código da Carteira</td>
+ * <td >9(2)</td>
+ * <td style="text-align:left;padding-left:10">Código da Carteira</td>
+ * <td style="text-align:left;padding-left:10">Código da Carteira</td>
  * </tr>
  * <tr>
  * <td >26-36</td>
  * <td >11</td>
- * <td >&nbsp;9(11) </td>
- * <td >Nosso Número (sem dígito)</td>
+ * <td >&nbsp;9(11)</td>
+ * <td style="text-align:left;padding-left:10">Número do Nosso Número(Sem o digito verificador)</td>
+ * <td style="text-align:left;padding-left:10">Nosso Número (sem dígito)</td>
  * </tr>
  * <tr>
  * <td >37-43</td>
  * <td >7</td>
- * <td >&nbsp;9(7) </td>
- * <td >Conta do Cedente (sem dígito)</td>
+ * <td >&nbsp;9(7)</td>
+ * <td style="text-align:left;padding-left:10">Conta do Cedente (Sem o digito verificador, completar com zeros a esquerda quando necessário)</td>
+ * <td style="text-align:left;padding-left:10">Conta do Cedente (sem dígito)</td>
  * </tr>
  * <tr>
  * <td >44-44</td>
  * <td >1</td>
- * <td >9 </td>
- * <td >Zero Fixo</td>
+ * <td >9</td>
+ * <td style="text-align:left;padding-left:10">Constante "0"</td>
+ * <td style="text-align:left;padding-left:10">Zero Fixo</td>
  * </tr>
  * </table>
  * 
@@ -84,9 +91,10 @@ import org.jrimum.utilix.text.Filler;
  * 
  * 
  * @author <a href="http://gilmatryx.googlepages.com/">Gilmar P.S.L</a>
- * @author <a href="mailto:misaelbarreto@gmail.com">Misael Barreto</a> 
+ * @author <a href="mailto:misaelbarreto@gmail.com">Misael Barreto</a>
  * @author <a href="mailto:romulomail@gmail.com">Rômulo Augusto</a>
- * @author <a href="http://www.nordestefomento.com.br">Nordeste Fomento Mercantil</a>
+ * @author <a href="http://www.nordestefomento.com.br">Nordeste Fomento
+ *         Mercantil</a>
  * 
  * @since 0.2
  * 
@@ -100,31 +108,35 @@ class CLBradesco extends AbstractCLBradesco {
 	private static final long serialVersionUID = -1253549781074159862L;
 
 	/**
-	 * 
+	 * Número de campos.
 	 */
-	private static final Integer FIELDS_LENGTH = 5;
+	private static final Integer FIELDS_LENGTH = Integer.valueOf(5);
 	
 	/**
 	 * <p>
 	 *   Dado um título, cria um campo livre para o padrão do Banco Bradesco.
 	 * </p>
-	 * @param titulo título com as informações para geração do campo livre
+	 * 
+	 * @param titulo - Título com as informações para geração do campo livre
 	 */
 	CLBradesco(Titulo titulo){
+		
 		super(FIELDS_LENGTH);
 		
-		ContaBancaria conta = titulo.getContaBancaria();
-		String nossoNumero = titulo.getNossoNumero();
+		checkAgenciaNotNull(titulo);
+		checkCodigoDaAgencia(titulo);
+		checkCarteiraNotNull(titulo);
+		checkCodigoDaCarteira(titulo);
+		checkNossoNumero(titulo);
+		checkTamanhoNossoNumero(titulo, NN11);
+		checkNumeroDaContaNotNull(titulo);
+		checkCodigoDoNumeroDaConta(titulo);
 		
-		this.add(new Field<Integer>(conta.getAgencia().getCodigo(), 4, Filler.ZERO_LEFT));
-		
-		this.add(new Field<Integer>(conta.getCarteira().getCodigo(), 2, Filler.ZERO_LEFT));
-		this.add(new Field<String>(nossoNumero, 11, Filler.ZERO_LEFT));
-		
-		this.add(new Field<Integer>(conta.getNumeroDaConta().getCodigoDaConta(), 7, Filler.ZERO_LEFT));
-		
+		this.add(new Field<Integer>(titulo.getContaBancaria().getAgencia().getCodigo(), 4, Filler.ZERO_LEFT));
+		this.add(new Field<Integer>(titulo.getContaBancaria().getCarteira().getCodigo(), 2, Filler.ZERO_LEFT));
+		this.add(new Field<String>(titulo.getNossoNumero(), 11, Filler.ZERO_LEFT));
+		this.add(new Field<Integer>(titulo.getContaBancaria().getNumeroDaConta().getCodigoDaConta(), 7, Filler.ZERO_LEFT));
 		this.add(new Field<Integer>(0, 1));
-		
 	}
 	
 }
