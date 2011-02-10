6d48029e84bd774042c426f89639333839ca1d55
Gilmar P.S.L <gilmatryx@gmail.com>
2011-02-10 06:34:10 +0000

    [https://groups.google.com/forum/?hl=pt#%21topic/jrimum-community/pmOsOyoP49w Convênio BB com 7]  * Rename para fazer distinção

--- src/main/java/org/jrimum/bopepo/campolivre/CLBancoDoBrasilNN17Convenio6.java
+++ src/main/java/org/jrimum/bopepo/campolivre/CLBancoDoBrasilNN17Convenio6.java
@@ -37,36 +37,37 @@ import org.jrimum.utilix.text.Filler;
 
 /**
  * 
- * O campo livre do Banco do Brasil com o nosso número de 17 dígitos deve seguir
- * esta forma:
+ * O campo livre do Banco do Brasil com o nosso número de 17 dígitos e convênio
+ * de 7 posições deve seguir esta forma:
  * 
- * <table border="1" cellpadding="0" cellspacing="0" style="border-collapse:
- * collapse" bordercolor="#111111" width="60%" id="campolivre">
- * <tr> <thead>
- * <th>Posição </th>
+ * <table border="1" cellpadding="0" cellspacing="0" style="border-collapse: * collapse" bordercolor="#111111" width="60%" id="campolivre">
+ * <tr>
+ * <thead>
+ * <th>Posição</th>
  * <th>Tamanho</th>
  * <th>Picture</th>
  * <th>Conteúdo (terminologia padrão)</th>
  * <th>Conteúdo (terminologia do banco)</th>
- * </thead> </tr>
+ * </thead>
+ * </tr>
  * <tr>
  * <td>20-25</td>
  * <td>6</td>
- * <td>9(6) </td>
+ * <td>9(6)</td>
  * <td>Conta do cedente (sem dígito)</td>
  * <td>Convênio (sem dígito)</td>
  * </tr>
  * <tr>
  * <td>26-42</td>
  * <td>17</td>
- * <td>9(17) </td>
+ * <td>9(17)</td>
  * <td>Nosso Número (sem dígito)</td>
  * <td>Nosso Número (sem dígito)</td>
  * </tr>
  * <tr>
  * <td>43-44</td>
  * <td>2</td>
- * <td>9(2) </td>
+ * <td>9(2)</td>
  * <td>Fixo 21 (serviço)</td>
  * <td>Fixo 21 (serviço)</td>
  * </tr>
@@ -84,7 +85,7 @@ import org.jrimum.utilix.text.Filler;
  * 
  * @version 0.2
  */
-class CLBancoDoBrasilNN17 extends AbstractCLBancoDoBrasil{
+class CLBancoDoBrasilNN17Convenio6 extends AbstractCLBancoDoBrasil{
 
 	/**
 	 * 
@@ -108,7 +109,7 @@ class CLBancoDoBrasilNN17 extends AbstractCLBancoDoBrasil{
 	 * </p>
 	 * @param titulo título com as informações para geração do campo livre
 	 */
-	CLBancoDoBrasilNN17(Titulo titulo) {
+	CLBancoDoBrasilNN17Convenio6(Titulo titulo) {
 		super(FIELDS_LENGTH);
 		
 		ContaBancaria conta = titulo.getContaBancaria();
