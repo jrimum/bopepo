0cf1faa0d28f90a28a02c12b32e27a5cd12f8c92
Gilmar P.S.L <gilmatryx@gmail.com>
2011-02-10 06:39:21 +0000

    #57  * Revisão de itens referente a padronização

--- src/main/java/org/jrimum/bopepo/campolivre/CLBancoDoNordesteDoBrasil.java
+++ src/main/java/org/jrimum/bopepo/campolivre/CLBancoDoNordesteDoBrasil.java
@@ -1,29 +1,30 @@
-/*
- * Copyright 2008 JRimum Project
+/* 
+ * Copyright 2010 JRimum Project
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
- * the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
- * applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
- * OF ANY KIND, either express or implied. See the License for the specific
- * language governing permissions and limitations under the License.
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  * 
- * Created at: 30/03/2008 - 18:09:27
- * 
+ * Created at: 16/12/2010 - 13:24:00
+ *
  * ================================================================================
+ *
+ * Direitos autorais 2010 JRimum Project
+ *
+ * Licenciado sob a Licença Apache, Versão 2.0 ("LICENÇA"); você não pode 
+ * usar esse arquivo exceto em conformidade com a esta LICENÇA. Você pode obter uma 
+ * cópia desta LICENÇA em http://www.apache.org/licenses/LICENSE-2.0 A menos que 
+ * haja exigência legal ou acordo por escrito, a distribuição de software sob esta 
+ * LICENÇA se dará “COMO ESTÁ”, SEM GARANTIAS OU CONDIÇÕES DE QUALQUER TIPO, sejam 
+ * expressas ou tácitas. Veja a LICENÇA para a redação específica a reger permissões 
+ * e limitações sob esta LICENÇA.
  * 
- * Direitos autorais 2008 JRimum Project
- * 
- * Licenciado sob a Licença Apache, Versão 2.0 ("LICENÇA"); você não pode usar
- * esse arquivo exceto em conformidade com a esta LICENÇA. Você pode obter uma
- * cópia desta LICENÇA em http://www.apache.org/licenses/LICENSE-2.0 A menos que
- * haja exigência legal ou acordo por escrito, a distribuição de software sob
- * esta LICENÇA se dará “COMO ESTÁ”, SEM GARANTIAS OU CONDIÇÕES DE QUALQUER
- * TIPO, sejam expressas ou tácitas. Veja a LICENÇA para a redação específica a
- * reger permissões e limitações sob esta LICENÇA.
- * 
- * Criado em: 30/03/2008 - 18:09:27
+ * Created at: 16/12/2010 - 13:24:00
  * 
  */
 
@@ -103,6 +104,7 @@ import org.jrimum.utilix.text.Filler;
  * 
  * @see org.jrimum.bopepo.campolivre.AbstractCampoLivre
  * 
+ * @author <a href="http://gilmatryx.googlepages.com/">Gilmar P.S.L.</a>
  * @author <a href="mailto:misaelbarreto@gmail.com">Misael Barreto</a>
  * @author <a href="mailto:pporto@gmail.com">Paulo Porto</a>
  * 
@@ -113,9 +115,9 @@ import org.jrimum.utilix.text.Filler;
 class CLBancoDoNordesteDoBrasil extends AbstractCLBancoDoNordesteDoBrasil {
 	
 	/**
-	 *  {@code serialVersionUID = -6487723647496876064L}
+	 * {@code serialVersionUID = 5203223333877548162L}
 	 */
-	private static final long serialVersionUID = -6487723647496876064L;
+	private static final long serialVersionUID = 5203223333877548162L;
 
 	/**
 	 * Número de campos = 7.
@@ -181,35 +183,26 @@ class CLBancoDoNordesteDoBrasil extends AbstractCLBancoDoNordesteDoBrasil {
 		checkAgenciaNotNull(titulo);
 		checkCodigoDaAgencia(titulo);
 		checkCodigoDaAgenciaMenorOuIgualQue(titulo, 9999);
-		
 		checkNumeroDaContaNotNull(titulo);
 		checkCodigoDoNumeroDaConta(titulo);
 		checkCodigoDoNumeroDaContaMenorOuIgualQue(titulo, 9999999);
 		checkDigitoDoCodigoDoNumeroDaConta(titulo);
-
 		checkNossoNumero(titulo);
 		checkDigitoDoNossoNumero(titulo);
-
 		checkCarteiraNotNull(titulo);
 		checkCodigoDaCarteira(titulo);
 		checkCodigoDaCarteiraMenorOuIgualQue(titulo, 99);
-		
 	}
 	
 	@Override
 	protected void addFields(Titulo titulo) {
 		
 		this.add(new Field<Integer>(titulo.getContaBancaria().getAgencia().getCodigo(), AGENCIA_LENGTH, Filler.ZERO_LEFT));
-
 		this.add(new Field<Integer>(titulo.getContaBancaria().getNumeroDaConta().getCodigoDaConta(), CONTA_LENGTH, Filler.ZERO_LEFT));
 		this.add(new Field<String>(titulo.getContaBancaria().getNumeroDaConta().getDigitoDaConta(), DIGITO_CONTA_LENGTH, Filler.ZERO_LEFT));
-		
 		this.add(new Field<String>(titulo.getNossoNumero(), NOSSO_NUMERO_LENGTH, Filler.ZERO_LEFT));
 		this.add(new Field<String>(titulo.getDigitoDoNossoNumero(), DIGITO_NOSSO_NUMERO_LENGTH, Filler.ZERO_LEFT));
-		
 		this.add(new Field<Integer>(titulo.getContaBancaria().getCarteira().getCodigo(), CARTEIRA_LENGTH, Filler.ZERO_LEFT));
-		
-		this.add(new Field<Integer>(CAMPO_ZERADO_VALUE, CAMPO_ZERADO_LENGTH,  Filler.ZERO_LEFT));
-		
+		this.add(new Field<Integer>(CAMPO_ZERADO_VALUE, CAMPO_ZERADO_LENGTH,  Filler.ZERO_LEFT));		
 	}
 }
