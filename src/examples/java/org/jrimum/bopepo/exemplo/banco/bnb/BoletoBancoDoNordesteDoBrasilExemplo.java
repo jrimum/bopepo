e5b8db107d9118c1fb0e423ad5d06b44cb3057c2
Gilmar P.S.L <gilmatryx@gmail.com>
2011-02-10 06:40:00 +0000

    #57  * Revisão de itens referente a padronização

--- src/examples/java/org/jrimum/bopepo/exemplo/banco/bnb/BoletoBancoDoNordesteDoBrasilExemplo.java
+++ src/examples/java/org/jrimum/bopepo/exemplo/banco/bnb/BoletoBancoDoNordesteDoBrasilExemplo.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2008 JRimum Project
+ * Copyright 2010 JRimum Project
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -10,11 +10,11 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  * 
- * Created at: 16/09/2009 - 00:44:51
+ * Created at: 16/12/2010 - 13:24:00
  *
  * ================================================================================
  *
- * Direitos autorais 2008 JRimum Project
+ * Direitos autorais 2010 JRimum Project
  *
  * Licenciado sob a Licença Apache, Versão 2.0 ("LICENÇA"); você não pode 
  * usar esse arquivo exceto em conformidade com a esta LICENÇA. Você pode obter uma 
@@ -24,13 +24,13 @@
  * expressas ou tácitas. Veja a LICENÇA para a redação específica a reger permissões 
  * e limitações sob esta LICENÇA.
  * 
- * Created at: 16/09/2009 - 00:44:51
+ * Created at: 16/12/2010 - 13:24:00
  * 
  */
+
+
 package org.jrimum.bopepo.exemplo.banco.bnb;
 
-
-import java.math.BigDecimal;
 
 import org.jrimum.bopepo.BancosSuportados;
 import org.jrimum.bopepo.Boleto;
@@ -40,17 +40,17 @@ import org.jrimum.domkee.financeiro.banco.febraban.Carteira;
 import org.jrimum.domkee.financeiro.banco.febraban.ContaBancaria;
 import org.jrimum.domkee.financeiro.banco.febraban.NumeroDaConta;
 import org.jrimum.domkee.financeiro.banco.febraban.Titulo;
-import org.jrimum.utilix.text.DateFormat;
 
 /**
  * <p>
- * Exemplo do boleto para o Banco Bradesco
+ * Exemplo do boleto para o Banco do Nordeste
  * </p>
  * <p>
  * Mostra um exemplo funcional que gera um boleto para a implementação de campo livre
  * do Banco do Nordeste do Brasil
  * </p>
  * 
+ * @author <a href="http://gilmatryx.googlepages.com/">Gilmar P.S.L.</a>
  * @author <a href="mailto:misaelbarreto@gmail.com">Misael Barreto</a>
  * @author <a href="mailto:pporto@gmail.com">Paulo Porto</a>
  * 
@@ -63,28 +63,39 @@ public class BoletoBancoDoNordesteDoBrasilExemplo {
 		Titulo titulo = Exemplos.crieTitulo();
 		
 		/*
-		 * Campos específicos para o Banco Bradesco.
+		 * Campos específicos para o Banco do Nordeste.
 		 */
 		
 		ContaBancaria contaBancaria = titulo.getContaBancaria();
 		
-		//Banco bradesco 237
+		/*
+		 * Banco do Nordeste 004
+		 */
 		contaBancaria.setBanco(BancosSuportados.BANCO_DO_NORDESTE_DO_BRASIL.create());
 		
-		//Agência com no máximo 4 dígitos
+		/*
+		 * Agência com no máximo 4 dígitos
+		 */
 		contaBancaria.setAgencia(new Agencia(100));
 
-		//Carteira com no máximo 2 dígitos 
+		/*
+		 * Carteira com no máximo 2 dígitos 
+		 */
 		contaBancaria.setCarteira(new Carteira(57));
 		
-		//Nosso número com 7 dígitos
+		/*
+		 * Nosso Número com 7 dígitos
+		 */
 		titulo.setNossoNumero("76");
-		titulo.setDigitoDoNossoNumero("0");
-		titulo.setValor(new BigDecimal(400));
-		titulo.setDataDoDocumento(DateFormat.DDMMYYYY_B.parse("09/12/2010"));
-		titulo.setDataDoVencimento(DateFormat.DDMMYYYY_B.parse("09/12/2010"));
 		
-		//Número da conta com no máxiom 7 dígitos
+		/*
+		 * Dígito do Nosso Número
+		 */
+		titulo.setDigitoDoNossoNumero("0");
+		
+		/*
+		 * Número da conta com no máxiom 7 dígitos e necessariamente com o dígito verificador
+		 */
 		contaBancaria.setNumeroDaConta(new NumeroDaConta(13677, "9"));
 		
 		Boleto boleto = Exemplos.crieBoleto(titulo);
