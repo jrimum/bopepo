abef61ef1d8ae867ebbf4dfc41a2718ac68e6aaa
Gilmar P.S.L <gilmatryx@gmail.com>
2011-03-23 06:47:34 +0000

    Revisão de código  * Organize imports

--- src/main/java/org/jrimum/bopepo/pdf/Files.java
+++ src/main/java/org/jrimum/bopepo/pdf/Files.java
@@ -31,9 +31,11 @@ package org.jrimum.bopepo.pdf;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.jrimum.utilix.Objects;
@@ -48,8 +50,7 @@ import org.jrimum.utilix.text.Strings;
  * @author <a href="http://gilmatryx.googlepages.com/">Gilmar P.S.L</a>
  * @author <a href="mailto:misaelbarreto@gmail.com">Misael Barreto</a>
  * @author <a href="mailto:romulomail@gmail.com">Rômulo Augusto</a>
- * @author <a href="http://www.nordestefomento.com.br">Nordeste Fomento
- *         Mercantil</a>
+ * @author <a href="http://www.nordestefomento.com.br">Nordeste Fomento Mercantil</a>
  * 
  * @since 0.2
  * 
@@ -57,31 +58,36 @@ import org.jrimum.utilix.text.Strings;
  */
 public class Files {
 
+	private static final int DEFAULT_BUFFER_SIZE = 4096;
+
 	/**
 	 * <p>
 	 * Transforma um array de bytes em um arquivo.
 	 * </p>
 	 * 
-	 * @param pathName - Caminho do arquivo para onde os bytes serão escritos.
-	 * @param bytes - Bytes a serem copiados.
+	 * @param pathName
+	 *            - Caminho do arquivo para onde os bytes serão escritos.
+	 * @param bytes
+	 *            - Bytes a serem copiados.
 	 * 
 	 * @return Objeto File com o conteúdo sendo o dos bytes
 	 * 
 	 * @throws FileNotFoundException
 	 * @throws IOException
-	 * @throws NullPointerException Caso <code>pathName</code> ou <code>bytes</code> sejam <code>null</code>.
-	 * @throws IllegalArgumentException Caso <code>pathName</code> seja vazio ou contenha apenas espaços em branco
-	 * 
+	 * @throws IllegalArgumentException
+	 *             Caso {@codepathName} ou {@code bytes} seja null, vazio ou
+	 *             contenha apenas espaços em branco
 	 * @since 0.2
 	 */
-	public static File bytesToFile(String pathName, byte[] bytes) throws FileNotFoundException, IOException {
+	public static File bytesToFile(String pathName, byte[] bytes)
+			throws FileNotFoundException, IOException {
 
 		Strings.checkNotBlank(pathName);
 		Objects.checkNotNull(bytes);
 
 		return bytesToFile(new File(pathName), bytes);
 	}
-	
+
 	/**
 	 * <p>
 	 * Transforma um array de bytes em um arquivo.
@@ -96,24 +102,24 @@ public class Files {
 	 * 
 	 * @throws FileNotFoundException
 	 * @throws IOException
-	 * @throws NullPointerException
-	 *             Caso <code>pathName</code> ou <code>bytes</code> sejam
-	 *             <code>null</code>.
 	 * @throws IllegalArgumentException
-	 *             Caso <code>pathName</code> seja vazio ou contenha apenas
-	 *             espaços em branco
+	 *             Caso {@codepathName} ou {@code bytes} seja null, vazio ou
+	 *             contenha apenas espaços em branco
 	 * 
 	 * @since 0.2
 	 */
-	public static File bytesToFile(File file, byte[] bytes) throws FileNotFoundException, IOException {
+	public static File bytesToFile(File file, byte[] bytes)
+			throws FileNotFoundException, IOException {
 
 		Objects.checkNotNull(file);
 		Objects.checkNotNull(bytes);
 
-		if(file.length() > Integer.MAX_VALUE){
-			throw new IllegalArgumentException("TAMANHO DO ARQUIVO MAIOR DO QUE O SUPORTADO: "+Integer.MAX_VALUE);
+		if (file.length() > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException(
+					"TAMANHO DO ARQUIVO MAIOR DO QUE O SUPORTADO: "
+							+ Integer.MAX_VALUE);
 		}
-		
+
 		OutputStream out = new FileOutputStream(file);
 
 		out.write(bytes);
@@ -128,16 +134,19 @@ public class Files {
 	 * Transforma um array de bytes em um <code>ByteArrayOutputStream</code>.
 	 * </p>
 	 * 
-	 * @param bytes - Bytes que serão escritos no objeto ByteArrayOutputStream
+	 * @param bytes
+	 *            - Bytes que serão escritos no objeto ByteArrayOutputStream
 	 * 
 	 * @return ByteArrayOutputStream ou null
 	 * 
 	 * @throws IOException
-	 * @throws NullPointerException Caso <code>bytes</code> sejam <code>null</code>.
+	 * @throws IllegalArgumentException
+	 *             Caso os {@code bytes} sejam {@code null}.
 	 * 
 	 * @since 0.2
 	 */
-	public static ByteArrayOutputStream bytesToStream(byte[] bytes) throws IOException {
+	public static ByteArrayOutputStream bytesToStream(byte[] bytes)
+			throws IOException {
 
 		Objects.checkNotNull(bytes);
 
@@ -145,5 +154,68 @@ public class Files {
 		byteOut.write(bytes);
 
 		return byteOut;
+	}
+
+	/**
+	 * Retorna o conteúdo do arquivo em um array de bytes.
+	 * 
+	 * @param file
+	 * @return Conteúdo em um array de bytes.
+	 * @throws IOException
+	 * @throws IllegalArgumentException
+	 *             Caso o {@code file} seja {@code null}.
+	 */
+	public static byte[] fileToBytes(File file) throws IOException {
+
+		Objects.checkNotNull(file);
+
+		InputStream is = new FileInputStream(file);
+
+		byte[] bytes = new byte[(int) file.length()];
+
+		int offset = 0;
+		int numRead = 0;
+
+		while ((offset < bytes.length)
+				&& ((numRead = is.read(bytes, offset, bytes.length - offset)) >= 0)) {
+			offset += numRead;
+		}
+
+		is.close();
+
+		Objects.checkArgument(offset == bytes.length,
+				"Não foi possível completar a leitura do arquivo: "
+						+ file.getName());
+
+		return bytes;
+	}
+
+	/**
+	 * Retorna o conteúdo do {@code InputStream} em um array de bytes.
+	 * 
+	 * @param input
+	 * @return Conteúdo em um array de bytes.
+	 * @throws IOException
+	 * @throws IllegalArgumentException
+	 *             Caso o {@code input} seja {@code null}.
+	 */
+	public static byte[] toByteArray(InputStream input) throws IOException {
+
+		Objects.checkNotNull(input);
+
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+		byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+
+		int count = 0;
+		int n = 0;
+
+		while (-1 != (n = input.read(buffer))) {
+
+			output.write(buffer, 0, n);
+			count += n;
+		}
+
+		return output.toByteArray();
 	}
 }
