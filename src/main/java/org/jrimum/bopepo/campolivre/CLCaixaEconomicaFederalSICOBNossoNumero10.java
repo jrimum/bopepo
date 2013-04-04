dd12411af01e605931235f319e35fc4c6918cb4f
Misael Barreto <misaelbarreto@gmail.com>
2013-04-04 22:08:30 +0000

    [Bopepo] * Ajuste do campo livre "CLCaixaEconomicaFederalSICOBNossoNumero10" para permitir nosso número iniciando com 3. Obs: Ajuste em homologação. Aguardando resposta dos colegas da Neogrid.

--- src/main/java/org/jrimum/bopepo/campolivre/CLCaixaEconomicaFederalSICOBNossoNumero10.java
+++ src/main/java/org/jrimum/bopepo/campolivre/CLCaixaEconomicaFederalSICOBNossoNumero10.java
@@ -2,6 +2,7 @@ package org.jrimum.bopepo.campolivre;
 
 import static java.lang.String.format;
 
+import org.jrimum.domkee.financeiro.banco.febraban.Carteira;
 import org.jrimum.domkee.financeiro.banco.febraban.Titulo;
 import org.jrimum.utilix.Exceptions;
 import org.jrimum.utilix.Objects;
@@ -10,7 +11,7 @@ import org.jrimum.utilix.text.Filler;
 
 /**
  * <p>
- * O campo livre da Caixa Econômica Federal para cobrança rápida e sem registro
+ * O campo livre da Caixa Econômica Federal para cobrança simples (CS), rápida(CR) e sem registro (SR)
  * - SICOB, deve seguir esta forma:
  * </p>
  * 
@@ -29,8 +30,17 @@ import org.jrimum.utilix.text.Filler;
  * <td>10</td>
  * <td>9(10)</td>
  * <td style="text-align:left;padding-left:10px">Nosso Número</td>
- * <td style="text-align:left;padding-left:10">Nosso Número no padrão de uma das
- * duas cobranças (rápida ou sem registro)</td>
+ * <td style="text-align:left;padding-left:10">
+ * Nosso Número no padrão de uma das
+ * três cobranças (simples, rápida ou sem registro)
+ * 
+ * <p>Exemplos:<br/>
+ * Cobrança Simples - Nosso número inicia com 3. Ex: Carteira 11<br/>
+ * Cobrança Rápida - Nosso número inicia com 9. Ex: Carteira 12 <br/>
+ * Cobrança Sem Registro - Nosso número inicia com 80, 81 ou 82. Ex: Carteira 14.
+ * </p>
+ * 
+ * </td>
  * </tr>
  * <tr>
  * <td>30-33</td>
@@ -58,6 +68,10 @@ import org.jrimum.utilix.text.Filler;
  * 
  * @author <a href="http://gilmatryx.googlepages.com/">Gilmar P.S.L.</a>
  * @author <a href="mailto:misaelbarreto@gmail.com">Misael Barreto</a>
+
+ * @see <a href="http://www1.caixa.gov.br/download/asp/download.asp?subCategId=198&CategId=65&subCateglayout=Manuais&Categlayout=Cobran%C3%A7a%20Caixa%20%E2%80%93%20SICOB#aba_voce">Manuais SICOB - Caixa</a>
+ * @see <a href="http://downloads.caixa.gov.br/_arquivos/cobrcaixasicob/manuaissicob/ESPCODBARR_SICOB.pdf">Especificação código barras com nosso número de 11 posições</a>
+ * @see <a href="http://downloads.caixa.gov.br/_arquivos/cobrcaixasicob/manuaissicob/ESPCODBARBLOQCOBRANREGIST_16POSICOES.pdf">Leiaute de Arquivo Eletrônico Padrão CNAB 240 - Cobrança Bancária CAIXA - SICOB</a>
  * 
  * @since 0.2
  * 
@@ -90,6 +104,14 @@ class CLCaixaEconomicaFederalSICOBNossoNumero10 extends AbstractCLCaixaEconomica
 		Objects.checkNotNull(titulo.getParametrosBancarios(), "Parâmetros bancários necessários [titulo.getParametrosBancarios()==null]!");
 		checkPadraoNossoNumero(titulo.getNossoNumero());
 		
+		// TODO: Testar checkPadraoNossoNumeroPorCodigoDaCarteira;
+		/*
+		Integer codigoDaCarteira = titulo.getContaBancaria().getCarteira().getCodigo(); 
+		if (Objects.isNotNull(codigoDaCarteira)) {
+			checkPadraoNossoNumeroPorCodigoDaCarteira(titulo.getNossoNumero(), codigoDaCarteira);
+		}
+		*/
+		
 		this.add(new Field<String>(titulo.getNossoNumero(), 10));
 	
 		this.add(new Field<Integer>(titulo.getContaBancaria().getAgencia().getCodigo(), 4, Filler.ZERO_LEFT));
@@ -113,9 +135,10 @@ class CLCaixaEconomicaFederalSICOBNossoNumero10 extends AbstractCLCaixaEconomica
 	
 	/**
 	 * <p>
-	 * Verifica se o nosso número do título começa com 9 (identificador da
-	 * Carteira Rápida) ou, 80, 81 ou 82 (que são identificadores da Carteira
-	 * Sem Registro); Caso contrário gera uma {@code IllegalArgumentException}.
+	 * Verifica se o nosso número do título começa com 3 (identificador da
+	 * Carteira Simples), 9 (identificador da Carteira Rápida) ou 80, 81 ou 82
+	 * (que são identificadores da Carteira Sem Registro); Caso contrário gera
+	 * uma {@code IllegalArgumentException}.
 	 * </p>
 	 * 
 	 * <p>
@@ -133,11 +156,52 @@ class CLCaixaEconomicaFederalSICOBNossoNumero10 extends AbstractCLCaixaEconomica
 	 *            - Nosso Número
 	 */
 	private void checkPadraoNossoNumero(String nn){
-		
 		if(!nn.startsWith("3") && !nn.startsWith("9") && !nn.startsWith("80") && !nn.startsWith("81") && !nn.startsWith("82")){
-			Exceptions.throwIllegalArgumentException(format("Para a cobrança SICOB o nosso número [%s] deve começar com 9 que é o identificador da \"carteira rápida\" [9NNNNNNNNN] ou 80, 81 e 82 para \"carteira sem registro\" [82NNNNNNNN]!", nn));
+			Exceptions.throwIllegalArgumentException(format("Para a cobrança SICOB o nosso número [%s] deve começar com 3 que é o identificador da \"carteira siples\" [3NNNNNNNNN] ou 9 que é o identificador da \"carteira rápida\" [9NNNNNNNNN] ou 80, 81 e 82 para \"carteira sem registro\" [82NNNNNNNN]!", nn));
 		}
 	}
+	
+	/**
+	 * <p>
+	 * Verifica se o nosso número informado tem o padrão esperado de acordo com
+	 * o código da carteira informada.
+	 * </p>
+	 * <p>
+	 * Obs: Como não há uma garantia documental de que todo nosso número que
+	 * começa com "3" pertencerá <u>somente</u> a carteira 11, então o mais
+	 * coerente é não amarrar o início do nosso número a determinada carteira,
+	 * mas sim o contrário, ou seja, amarrar que toda carteira 11 tem de ter o
+	 * nosso número iniciando com 3". O mesmo raciocíno serve para as outras
+	 * carteiras.
+	 * </p>
+	 * 
+	 * @param nossoNumero
+	 *            - Nosso Número
+	 * @param codigoDaCarteira
+	 *            - Código da carteira
+	 */
+	private void checkPadraoNossoNumeroPorCodigoDaCarteira(String nossoNumero, Integer codigoDaCarteira){
+		switch (codigoDaCarteira) {
+		case 11:
+			if(!nossoNumero.startsWith("3")){
+				Exceptions.throwIllegalArgumentException(format("Para a cobrança SICOB, carteira 11 (cobrança simples), o nosso número [%s] deve começar com 3!", nossoNumero));
+			}
+			break;
+
+		case 12:
+			if(!nossoNumero.startsWith("9")){
+				Exceptions.throwIllegalArgumentException(format("Para a cobrança SICOB, carteira 12 (cobrança rápida), o nosso número [%s] deve começar com 9!", nossoNumero));
+			}
+			break;
+					
+		case 14:
+			if(!nossoNumero.startsWith("80") && !nossoNumero.startsWith("81") && !nossoNumero.startsWith("82")){
+				Exceptions.throwIllegalArgumentException(format("Para a cobrança SICOB, carteira 14 (cobrança sem registro), o nosso número [%s] deve começar com 80, 81 ou 82!", nossoNumero));
+			}
+			break;
+		}
+	}
+	
 	
 	@Override
 	protected void addFields(Titulo titulo) {
