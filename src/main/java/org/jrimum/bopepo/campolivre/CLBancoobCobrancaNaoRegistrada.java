7fa8a6eeb54c6f620c2b2005b8c1c895cb99a8d8
Gilmar P.S.L <gilmatryx@gmail.com>
2013-09-06 11:38:03 -0300

    Organizando imports

--- src/main/java/org/jrimum/bopepo/campolivre/CLBancoobCobrancaNaoRegistrada.java
+++ src/main/java/org/jrimum/bopepo/campolivre/CLBancoobCobrancaNaoRegistrada.java
@@ -28,7 +28,6 @@
 
 package org.jrimum.bopepo.campolivre;
 
-import org.jrimum.domkee.financeiro.banco.ParametrosBancariosMap;
 import org.jrimum.domkee.financeiro.banco.febraban.Titulo;
 import org.jrimum.utilix.text.Field;
 import org.jrimum.utilix.text.Filler;
