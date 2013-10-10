b0f1f636e5dcc32132364dd9484d3f3d070a9e1c
Gilmar P.S.L <gilmatryx@gmail.com>
2013-10-10 00:34:21 -0300

    OPEN - task 1: Mudar passagem de parâmetros bancários de string para enum  * Javadoc

--- src/main/java/org/jrimum/bopepo/parametro/ParametroBancoSicredi.java
+++ src/main/java/org/jrimum/bopepo/parametro/ParametroBancoSicredi.java
@@ -30,8 +30,20 @@
 
 package org.jrimum.bopepo.parametro;
 
+import org.jrimum.bopepo.BancosSuportados;
 import org.jrimum.domkee.financeiro.banco.ParametroBancario;
 
+
+/**
+ * Parâmetros bancários específicos para o
+ * {@link BancosSuportados#BANCO_SICREDI}.
+ * 
+ * @author <a href="http://gilmatryx.googlepages.com/">Gilmar P.S.L.</a>
+ * 
+ * @version 0.2.3
+ * 
+ * @since 0.2
+ */
 public enum ParametroBancoSicredi implements ParametroBancario<ParametroBancoSicredi>{
 	/**
 	 * Nome do parâmetro bancário contendo o valor do posto da agência SICREDI.
