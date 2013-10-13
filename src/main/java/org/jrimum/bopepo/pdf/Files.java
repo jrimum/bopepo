419370dfe30fcf9259e84cb564463f981425e32c
Gilmar P.S.L. <gilmatryx@gmail.com>
2013-10-13 19:38:48 -0300

    Removendo variável não utilizada.

--- src/main/java/org/jrimum/bopepo/pdf/Files.java
+++ src/main/java/org/jrimum/bopepo/pdf/Files.java
@@ -234,13 +234,11 @@ public class Files {
 
 		byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
 
-		int count = 0;
 		int n = 0;
 
 		while (-1 != (n = input.read(buffer))) {
 
 			output.write(buffer, 0, n);
-			count += n;
 		}
 		
 		input.close();
