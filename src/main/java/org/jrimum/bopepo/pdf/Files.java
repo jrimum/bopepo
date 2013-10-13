31c5c173085aee48a8f1fc9db9b74e6abbfcad6f
Gilmar P.S.L. <gilmatryx@gmail.com>
2013-10-13 19:51:44 -0300

    Colocando o input stream para fechar na ordem correta.

--- src/main/java/org/jrimum/bopepo/pdf/Files.java
+++ src/main/java/org/jrimum/bopepo/pdf/Files.java
@@ -389,21 +389,14 @@ public class Files {
     		
 	        InputStream is = new FileInputStream(file);
 	    
-	        // Get the size of the file
 	        long length = file.length();
 	    
-	        // You cannot create an array using a long type.
-	        // It needs to be an int type.
-	        // Before converting to an int type, check
-	        // to ensure that file is not larger than Integer.MAX_VALUE.
 	        if (length > Integer.MAX_VALUE) {
-	            // File is too large
+	        	Exceptions.throwIllegalArgumentException(String.format("File is too large! Max file length capacity is %s bytes.",length));
 	        }
 	    
-	        // Create the byte array to hold the data
 	        byte[] bytes = new byte[(int)length];
 	    
-	        // Read in the bytes
 	        int offset = 0;
 	        int numRead = 0;
 	        while ((offset < bytes.length)
@@ -411,18 +404,15 @@ public class Files {
 	            offset += numRead;
 	        }
 	    
-	        // Ensure all the bytes have been read in
+	        is.close();
+	        
 	        if (offset < bytes.length) {
 	            throw new IOException("Could not completely read file "+file.getName());
 	        }
 	    
-	        // Close the input stream and return bytes
-	        is.close();
-	        
 	        return bytes;
 	        
     	}catch (Exception e) {
-    		
 			return Exceptions.throwIllegalStateException(e);
 		}
     }
