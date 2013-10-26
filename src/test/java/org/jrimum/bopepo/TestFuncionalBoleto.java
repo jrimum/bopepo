13830254d386beed5dac290bb97279e69ae29b9c
Gilmar P.S.L <gilmatryx@gmail.com>
2013-10-26 18:44:05 -0200

    Classe para testes funcionais no Bopepo

---
+++ src/test/java/org/jrimum/bopepo/TestFuncionalBoleto.java
@@ -0,0 +1,13 @@
+package org.jrimum.bopepo;
+
+public class TestFuncionalBoleto {
+	
+	//criar boleto objeto
+	//crriar boleto viewr
+	//gerar pdf sem remover campos
+	//pegar pdf
+	//ler todos os campos texto
+	//ler todos os campos imagem
+	//fazer asserts
+
+}
